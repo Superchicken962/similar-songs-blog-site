ae56f11fcd589ee06c8b24e3fd47662b588fda36
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2025-05-16 21:15:40 +0930

    Add styling for song comparisons + a bit of data

---
+++ js/Song.js
@@ -0,0 +1,38 @@
+class Song {
+    constructor(name, author, duration, releaseYear, releaseMonth) {
+        this.name = name;
+        this.author = author;
+        this.duration = duration;
+        this.release = {
+            year: releaseYear,
+            month: releaseMonth
+        };
+    }
+
+    /**
+     * Create a song class from a generic object.
+     * 
+     * @param { Object } obj 
+     */
+    static fromObj(obj) {
+        return new Song(obj.name, obj.author, obj.duration, obj.release?.year, obj.release?.month);
+    }
+
+    formatDuration() {
+        const seconds = this.duration%60;
+        const minutes = Math.floor(this.duration/60);
+
+        return `${minutes}m ${seconds}s`;
+    }
+
+    formatRelease() {
+        const date = new Date();
+        date.setFullYear(this.release.year);
+        if (this.release.month) date.setMonth(this.release.month-1);
+
+        return date.toLocaleString("en-AU", {
+            year: "numeric",
+            month: "long"
+        });
+    }
+}
--- js/songs.js
+++ js/songs.js
@@ -1,21 +1,89 @@
 const messageBox = document.querySelector(".messageDisplay");
 
 async function loadData() {
-    const req = await fetch("songs.json");
-    let data;
+    const reqSongs = await fetch("songs.json");
+    const reqComparisons = await fetch("comparisons.json");
+    let songs;
+    let comparisons;
 
     try {
-        data = await req.json();
+        songs = await reqSongs.json();
+        comparisons = await reqComparisons.json();
     } catch (error) {
+        messageBox.setMessage("Oops! There was an error loading the data!", "error");
         console.warn(error);
+        return;
     }
 
-    if (data.length === 0) {
+    if (comparisons.length === 0) {
         messageBox.setMessage("Oops! There were no songs found!", "warning");
         return;
     }
 
-    console.log(data);
+    const songsContainer = document.querySelector(".songs");
+
+    for (const comparison of comparisons) {
+        const { songA: songA_, songB: songB_, description } = comparison;
+
+        const times = {
+            songA: {
+                start: songA_.startAt,
+                end: songA_.endAt
+            },
+
+            songB: {
+                start: songB_.startAt,
+                end: songB_.endAt
+            }
+        };
+
+        const findSongA = songs.find(s => s.id === songA_.id);
+        const findSongB = songs.find(s => s.id === songB_.id);
+
+        if (!findSongA || !findSongB) continue;
+
+        const songA = Song.fromObj(findSongA);
+        const songB = Song.fromObj(findSongB);
+
+        const element = buildComparisonElement(songA, songB, { times, description });
+
+        songsContainer.appendChild(element);
+    }
+}
+
+/**
+ * 
+ * @param { Song } songA 
+ * @param { Song } songB 
+ * @param { Object } info 
+ * @returns { HTMLElement }
+ */
+function buildComparisonElement(songA, songB, info) {
+    console.log(songA);
+    console.log(songB);
+
+    const element = document.createElement("div");
+    element.className = "songComparison";
+
+    element.innerHTML = `
+        <div class="first song">
+            <h3>${songA.author} - ${songA.name}</h3>
+            <p class="release">${songA.formatRelease()}</p>
+            <p class="duration">${songA.formatDuration()}</p>
+        </div>
+
+        <div class="second song">
+            <h3>${songB.author} - ${songB.name}</h3>
+            <p class="release">${songB.formatRelease()}</p>
+            <p class="duration">${songB.formatDuration()}</p>
+        </div>
+
+        <div class="details">
+            <p class="description">${info.description}</p>
+        </div>
+    `;
+
+    return element;
 }
 
 loadData();
